4b19ff609319cf1b5e524f13d5bea57bfe5e4b37
Julia Akemy <144185022+Akemyk@users.noreply.github.com>
2024-09-30 19:31:34 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let a = Number(prompt("Digite um número(A)"))
+let b = Number(prompt("Digite outro número(B)"))
+let c = Number(prompt("Digite outro número(C)"))
+let d = Number(prompt("Digite outro número(D)"))
+
+let somaAB = a+b
+let multAB = a*b
+alert("A soma de A+B é "+somaAB)
+alert("A multiplicação de A*B é "+multAB)
+
+let somaAC = a+c
+let multAC = a*c
+alert("A soma de A+C é "+somaAC)
+alert("A multiplicação de A*C é "+multAC)
+
+let somaAD = a+d
+let multAD = a*d
+alert("A soma de A+C é "+somaAD)
+alert("A multiplicação de A*C é "+multAD)
+
+let somaBC = b+c
+let multBC = b*d
+alert("A soma de B*C é "+somaBC)
+alert("A multiplicação de B*C é "+multBC)
+
+let somaBD = b+d
+let multBD = b*d
+alert("A soma de B*C é "+somaBD)
+alert("A multiplicação de B*C é "+multBD)
